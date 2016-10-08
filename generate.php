ae9621ca24e0574ad518eb71db1f8f193b7256db
Maciej Mardosz <mmardosz@users.noreply.github.com>
2016-10-08 15:26:38 -0500

    Update generate.php

--- generate.php
+++ generate.php
@@ -35,34 +35,6 @@ echo '<h3>B-105</h3><table class="table table-condensed ">';
 fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=194");
 echo '</table>';
 
-echo '<h3>CBM-114</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=362");
-echo '</table>';
-
-echo '<h3>CBM-158</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=368");
-echo '</table>';
-
-echo '<h3>LWH-2108</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=330");
-echo '</table>';
-
-echo '<h3>LWH-2054</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=323");
-echo '</table>';
-
-echo '<h3>FA-104</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=77");
-echo '</table>';
-
-echo '<h3>FA-108</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=79");
-echo '</table>';
-
-echo '<h3>BBH-242</h3><table class="table table-condensed ">';
-fetch_location("https://25live.collegenet.com/25live/data/neiu/run/reservations.xml?space_id=493");
-echo '</table>';
-
 echo "<h3>
 		Data from <a href='https://25live.collegenet.com/25live/data/neiu/'>https://25live.collegenet.com/25live/data/neiu/</a> - Report generated at " . date("h:i:s A") . " for " . date("l, M d (d/m/Y)") . " - mmmardo1</h3>";
 
